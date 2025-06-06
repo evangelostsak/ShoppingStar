486ef7dc78e580c286cff498d0dd1fbbde8da010
evangelostsak <baggtsak55@gmail.com>
2025-06-06 16:27:11 +0200

    Interface for User and Item models

---
+++ backend/data_manager/interface.py
@@ -0,0 +1,58 @@
+from abc import ABC, abstractmethod
+
+class DataManagerInterface(ABC):
+
+    @abstractmethod
+    def get_all_users(self):
+        """Retrieve data based on a query."""
+        pass
+
+    @abstractmethod
+    def get_user(self, user_id):
+        """Get User by ID."""
+        pass
+
+    @abstractmethod
+    def register(self, username, password):
+        """Save data to the database."""
+        pass
+
+    @abstractmethod
+    def update_user(self, user_id):
+        """Update existing User's data identified by ID."""
+        pass
+
+    @abstractmethod
+    def delete_user(self, user_id):
+        """Delete User identified by ID."""
+        pass
+
+    @abstractmethod
+    def get_all_items(self):
+        """Retrieve all items."""
+        pass
+
+    @abstractmethod
+    def get_item(self, item_id):
+        """Get Item by ID."""
+        pass
+    
+    @abstractmethod
+    def add_item(self, name, description, price, rating, image):
+        """Add a new item."""
+        pass
+
+    @abstractmethod
+    def update_item(self, item_id, name, description, price, rating, image):
+        """Update existing Item's data identified by ID."""
+        pass
+
+    @abstractmethod
+    def delete_item(self, item_id):
+        """Delete Item identified by ID."""
+        pass
+
+    @abstractmethod
+    def like_item(self, item_id, user_id):
+        """Like an item."""
+        pass
